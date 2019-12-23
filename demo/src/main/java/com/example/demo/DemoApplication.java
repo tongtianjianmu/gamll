d9c593c6d7543f9bdbd8e2415378460741b7e50f
李家豪 <2044554625@qq.com>
2019-12-23 15:00:29 +0800

    连接测试版本1

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @Author: Li
+ * @Date: 2019/12/23 14:54
+ */
 @SpringBootApplication
 public class DemoApplication {
 
